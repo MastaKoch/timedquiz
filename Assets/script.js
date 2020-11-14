c767a04cd80c0e1beb07dc1845be8e3ce53f66d7
MastaKoch <alexkoch1996@gmail.com>
2020-11-14 16:51:49 -0500

    almost done.. just trying to get high scores to work

--- Assets/script.js
+++ Assets/script.js
@@ -1,72 +1,74 @@
+// questions array.. 5 questions total.
 var questions = [
-    {  
+    {
         // 0
-        question: "What does HTML stand for?",
+        question: "Question 1/5: What does HTML stand for?",
         answers: {
             a: "Happier Than My Lads",
             b: "Hyper Text Markup Language",
             c: "Hyper Text Macro Language",
             d: "Hyper Text Made-Up Language"
         },
-            correctAnswer: "b"
+        correctAnswer: "b"
     },
-    
+
     {
         // 1
-        question: "What does CSS stand for?",
+        question: "Question 2/5: What does CSS stand for?",
         answers: {
             a: "Cascading Style Sheets",
             b: "Crazy Style Sheets",
             c: "Cascading Swagger Sheets",
             d: "Casual Style Sheets"
         },
-            correctAnswer: "a"
+        correctAnswer: "a"
     },
-    
+
     {
         // 2
-        question: "Choose the correct HTML tag for the largest heading.",
+        question: "Question 3/5: Choose the correct HTML tag for the largest heading.",
         answers: {
             a: "<title>",
             b: "<head>",
             c: "<h1>",
             d: "<header>"
         },
-            correctAnswer: "c"
+        correctAnswer: "c"
     },
-    
+
     {
         // 3
-        question: "What is the correct HTML tag for inserting a line break?",
+        question: "Question 4/5: What is the correct HTML tag for inserting a line break?",
         answers: {
             a: "<skip>",
             b: "<break>",
             c: "<lb>",
             d: "<br>"
         },
-            correctAnswer: "d"
+        correctAnswer: "d"
     },
-    
+
     {
         // 4
-        question: "What are the three building blocks of web development?",
+        question: "Question 5/5: What are the three building blocks of web development? Click submit when finished",
         answers: {
             a: "HTML, CSS, Javascript",
             b: "HTML, CSS, Python",
             c: "CSS, Python, Javascript",
             d: "HTML, Python, Javascript"
         },
-            correctAnswer: "a"
+        correctAnswer: "a"
     },
-    
-    ]
+
+]
 
 // define all variables
 var startBtn = document.getElementById("startbtn");
 var quizContainer = document.getElementById("quiz");
 var submitButton = document.getElementById("score");
-var resultsContainer= document.getElementById("results");
+var resultsContainer = document.getElementById("results");
 var currentQuestion = 0;
+var timeLeft = 30;
 // var showQuestions = 0;
 var intro = document.getElementById("intro");
 var instructions = document.querySelector(".rules");
@@ -77,102 +79,162 @@ var instructions = document.querySelector(".rules");
 console.log(instructions);
 
 
-    // showQuestions function
-    function showQuestions(){
-        console.log("hi");
-        // removes HTML elements
-        instructions.textContent = "";
-        intro.textContent = "";
-        startBtn.remove();
+// beginQuiz function
+function beginQuiz() {
+
+    // removes HTML elements
+    instructions.textContent = "";
+    intro.textContent = "";
+    startbtn.remove();
+    highscorebtn.remove();
 
 
-        // adds timer when function showQuestions is executed
-        var para = document.createElement("p");
-        var node = document.createTextNode(15);
-        node.id= "timeLeft";
-        para.appendChild(node);
-        var element= document.getElementById("challenge");
-        element.appendChild(para);
+    // adds timer when function beginQuiz is executed
+    var para = document.createElement("p");
+    var node = document.createTextNode(30);
+    para.id = "timeLeft";
+    para.appendChild(node);
+    var element = document.getElementById("challenge");
+    element.appendChild(para);
+    startTimer();
 
 
-        var output=[];
-        var answers;
+    // creates "Next Question" button when function beginQuiz is executed
+    var nextQ = document.createElement("button");
+    var node2 = document.createTextNode("Submit");
+    nextQ.id = "submitBtn";
+    nextQ.appendChild(node2);
+    var nQ = document.getElementById("bottombtns");
+    nQ.appendChild(nextQ);
 
-        // creates H2 element to store questions and creates buttons to store answer choices..
-        var question = document.createElement("h2");
-        question.textContent = questions[currentQuestion].question
-        quizContainer.appendChild(question);
-        for (key in questions[currentQuestion].answers) {
-            console.log (questions[currentQuestion].answers[key]);
-            var answer = document.createElement("button");
-            answer.textContent = questions[currentQuestion].answers[key];
-            quizContainer.appendChild(answer);
+
+    // call resetTimer function (resets timer if you guess the correct answer)
+
+
+    var output = [];
+    var answers;
+    displayQuestion();
+
+
+}
+function displayQuestion() {
+    // creates H2 element to store questions and creates buttons to store answer choices..
+    quizContainer.innerHTML = "";
+    var question = document.createElement("h2");
+    question.textContent = questions[currentQuestion].question
+    quizContainer.appendChild(question);
+
+    // object loop
+    for (key in questions[currentQuestion].answers) {
+        console.log(questions[currentQuestion].answers[key]);
+        var answer = document.createElement("button");
+        answer.id = key;
+        answer.textContent = questions[currentQuestion].answers[key];
+        answer.addEventListener("click", checkAnswer);
+        quizContainer.appendChild(answer);
+    }
+}
+function checkAnswer(event) {
+
+    console.log(questions[currentQuestion].correctAnswer === event.target.id);
+    if (questions[currentQuestion].correctAnswer === event.target.id) {
+        currentQuestion++;
+        if (currentQuestion < questions.length) {
+            displayQuestion()
+        }
+        else {
+            showHighScores();
+        }
+    } else { 
+        timeLeft = timeLeft - 10 
+        currentQuestion++;
+        if (currentQuestion < questions.length) {
+            displayQuestion()
+        }
+        else {
+            showHighScores();
         }
     }
 
-function startTimer(){
-  // GET THIS WORKING *
-        // starts timer when showQuestions function is executed
-        
-            const timeLeftDisplay = document.getElementById("timeLeft")
-           
-            var timeLeft=15;
-                setInterval(function(){
-                     if(timeLeft <=0 ){
-                         clearInterval(timeLeft = 0)
-                     }
-                    timeLeftDisplay.innerHTML = timeLeft
-                    timeLeft -=1
-                }, 1000);
+
+
+}
+
+// [{
+//     initials: "JRJ",
+//     score: 5
+// }]
+function showHighScores() {
+
+
+}
+function startTimer() {
+    // starts timer when showQuestions function is executed
+
+    const timeLeftDisplay = document.getElementById("timeLeft")
+
+    // var timeLeft=5;
+    setInterval(function () {
+        if (timeLeft <= 0) {
+            clearInterval(timeLeft = 0)
+        }
+        timeLeftDisplay.innerHTML = timeLeft
+        timeLeft -= 1
+    }, 1000);
 };
 
 console.log(startBtn);
-    startBtn.addEventListener("click", showQuestions);
-   
-    
-function showResults(){
 
-    // code will go in here
-    // gather answer containers from our quiz
-    var answerContainers= quizContainer.querySelectorAll('.answers');
 
-    // keep track of answers
-    var userAnswer='';
-    var numCorrect=0;
+// When you click the start button, it will run showQuestions function.
+startBtn.addEventListener("click", beginQuiz);
 
-    // for each question...
-    for(var i=0; i<questions.length; i++){
+    // create resetTimer function, which resets the timer back to 5 seconds if you get the correct answer.
+//    function resetTimer(){
+//     if (userAnswer=== )
 
-        // find selected answer
-        userAnswer=(answerContainers[i].querySelector('input[name=question'+i+']:checked')|| {}).value;
+//    }
 
-        // if answer is correct
-        if(userAnswer===questions[i].correctAnswer){
-            // add to the number of correct answers
-            numCorrect++;
+// function showResults(){
 
-            // color the answers green
-            answerContainers[i].style.color= 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-            // color answers red
-            answerContainers[i].style.color= 'red';
-        }
-    }
-    // show number of correct answers out of total
-    resultsContainer.innerHTML=numCorrect + 'out of' + questions.length;
-}
+//     // code will go in here
+//     // gather answer containers from our quiz
+//     var answerContainers= quizContainer.querySelectorAll('.answers');
 
-// when user clicks submit, show results
-submitButton.onclick= function(){
-    showResults(questions, quizContainer, resultsContainer);
-    
-    }
-// when user clicks "Begin", quiz starts
-// startBtn.onclick=function(){
-//     generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton, startBtn);
+//     // keep track of answers
+//     var userAnswer='';
+//     var numCorrect=0;
+
+//     // for each question...
+//     for(var i=0; i<questions.length; i++){
+
+//         // find selected answer
+//         userAnswer=(answerContainers[i].querySelector('input[name=question'+i+']:checked')|| {}).value;
+
+//         // if answer is correct
+//         if(userAnswer===questions[i].correctAnswer){
+//             // add to the number of correct answers
+//             numCorrect++;
+
+//             // color the answers green
+//             answerContainers[i].style.color= 'lightgreen';
+//         }
+//         // if answer is wrong or blank
+//         else{
+//             // color answers red
+//             answerContainers[i].style.color= 'red';
+//         }
+//     }
+//     // show number of correct answers out of total
+//     resultsContainer.innerHTML=numCorrect + 'out of' + questions.length;
 // }
+
+// // when user clicks submit, show results
+// submitButton.onclick= function(){
+//     showResults(questions, quizContainer, resultsContainer);
+
+    // }
+
 
 
 

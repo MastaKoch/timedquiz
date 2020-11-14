090de3153305b193892c520f20029f672a6c7bce
MastaKoch <alexkoch1996@gmail.com>
2020-11-13 21:04:06 -0500

    making more tweaks to timer

--- Assets/script.js
+++ Assets/script.js
@@ -88,7 +88,8 @@ console.log(instructions);
 
         // adds timer when function showQuestions is executed
         var para = document.createElement("p");
-        var node = document.createTextNode(15, id="timeLeft");
+        var node = document.createTextNode(15);
+        node.id= "timeLeft";
         para.appendChild(node);
         var element= document.getElementById("challenge");
         element.appendChild(para);
@@ -112,11 +113,10 @@ console.log(instructions);
 function startTimer(){
   // GET THIS WORKING *
         // starts timer when showQuestions function is executed
-        ('DOMContentLoaded', () => {
+        
             const timeLeftDisplay = document.getElementById("timeLeft")
            
             var timeLeft=15;
-            function countDown(){
                 setInterval(function(){
                      if(timeLeft <=0 ){
                          clearInterval(timeLeft = 0)
@@ -124,10 +124,7 @@ function startTimer(){
                     timeLeftDisplay.innerHTML = timeLeft
                     timeLeft -=1
                 }, 1000);
-            }
-        });
-
-}
+};
 
 console.log(startBtn);
     startBtn.addEventListener("click", showQuestions);
